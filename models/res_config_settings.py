b6bf3d09353db48c00c18160d7b666d965d360f8
Aril Saputra <arilsaputra854@gmail.com>
2026-01-18 00:30:29 +0700

    fix: change whatsapp_message_template to Char field to fix res.config.settings error

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -16,7 +16,7 @@ class ResConfigSettings(models.TransientModel):
         help="The URL of the WAHA API endpoint (e.g., http://localhost:3000)."
     )
     
-    whatsapp_message_template = fields.Text(
+    whatsapp_message_template = fields.Char(
         string='WhatsApp Message Template',
         config_parameter='task_deadline_reminder.whatsapp_message_template',
         default="Reminder: Task '{{ task_name }}' is due on {{ deadline }}. Link: {{ task_link }}. Do not reply.",
